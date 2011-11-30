97be31e1bef5778f14f0909e9f36248053cd43b4
Alfredo Juarez <alfrekjv@gmail.com>
2011-11-30 11:21:28 -0600

    initializing the plugin structure

---
+++ config/slAssetPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * slAssetPlugin configuration.
+ * 
+ * @package     slAssetPlugin
+ * @subpackage  config
+ * @author      Senico Labs, LLC
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class slAssetPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
